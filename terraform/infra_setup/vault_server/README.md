a3a87a30953c2bd2e5bf6d9385ea5ad2bf4ee7c5
Joe Thompson <jthompson@hashicorp.com>
2023-02-02 09:42:34 -0500

    TF module warnings

---
+++ terraform/infra_setup/vault_server/README.md
@@ -0,0 +1,3 @@
+# vault_server
+
+## This module is suitable ONLY for demo use of Vault.  DO NOT use this to provision a Vault server in any real-world context as it takes shortcuts for ease of demonstration that make it unsuitable for deployment elsewhere.
--- terraform/infra_setup/vault_server/main.tf
+++ terraform/infra_setup/vault_server/main.tf
@@ -1,3 +1,7 @@
+/*
+This module is suitable ONLY for demo use of Vault.  DO NOT use this to provision a Vault server in any real-world context as it takes shortcuts for ease of demonstration that make it unsuitable for deployment elsewhere.
+*/
+
 terraform {
   required_providers {
     aws = {
